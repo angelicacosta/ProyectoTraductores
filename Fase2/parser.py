66b561010adcdbf5f9b4e2c3d29be6d7285938be
angelicacosta <14-10005@usb.ve>
2018-06-06 22:29:27 -0400

    Implementado formato de impresion
    Se modifico en parser.py los p[0] de cada clase para que se especificara el tipo de dato
    Se agrego archivo con casos de prueba

--- Fase2/parser.py
+++ Fase2/parser.py
@@ -2,11 +2,12 @@
 import ply.yacc as yacc
 import ply.lex as lex
 import sys
+import re
 from Entrega1 import tokens, obtenerColumna
 
 precedence = (
 	('left', 'TkSuma', 'TkResta'),
-    ('left', 'TkMult', 'TkDiv','TkMod'),
+	('left', 'TkMult', 'TkDiv','TkMod'),
 	#('right', 'UMINUS'),
 	('left', 'TkDisyuncion'),
 	('right', 'TkConjuncion'),
@@ -17,7 +18,7 @@ precedence = (
 	('left','TkConca'),
 	('left','TkShift'),
 	('left', 'TkCorcheteAbre', 'TkCorcheteCierra'),
-    ('nonassoc', 'TkMenor', 'TkMenorIgual','TkMayor','TkMayorIgual','TkIgual','TkDesigual'),
+	('nonassoc', 'TkMenor', 'TkMenorIgual','TkMayor','TkMayorIgual','TkIgual','TkDesigual'),
 	('left', 'TkParAbre','TkParCierra'),
 	('left', 'TkHacer')
 	#('right', 'UMINUS'),
@@ -31,10 +32,11 @@ class Node:
 		else:
 			self.children = [ ]
 		self.leaf = leaf
-		#print("type: "+self.type+", children[0]: "+str(children[0])+", children[1]: "+str(children[1])+", leaf: "+str(leaf))
-	
 	def __str__(self):
 		return str(self.type)
+
+	def changeType(self,newType):
+		self.type = newType
 
 def p_S(p):
 	''' S : TkWith Lista_Declaraciones Bloque_Inst 
@@ -99,16 +101,15 @@ def p_Inst(p):
 	p[0] = p[1]
 
 def p_Inst_Punto(p):
-	'''Inst_Punto : TkId TkPunto  TkPuntoYComa ''' ##Chequear
-	p[0]=Node(p[2], [p[1]],None )
+	'''Inst_Punto : TkId TkPunto TkPuntoYComa '''
+	p[0]=Node('PUNTO', [Node('Contenedor: '+p[1],None,None)],None )
 
 def p_Lista_Instrucciones(p):
 	'''Lista_Instrucciones : Inst 
 	|  Inst Lista_Instrucciones'''
 	if(len(p)>2):
-		#print('Secuenciacion \n')
-		p[0]=Node('Secuencia', [p[1],p[2]], None)
-		#p[0]= str(p[1]) +'\n' + str(p[2])  
+		p[0]=Node('SECUENCIACION', [p[1],p[2]], None)
+
 	else:
 		p[0] = p[1]
 	  
@@ -117,29 +118,58 @@ def p_Inst_If(p):
 	'''Inst_If : TkIf Operacion TkHacer Lista_Instrucciones TkEnd
 	| TkIf Operacion TkHacer Lista_Instrucciones TkOtherwise TkHacer Lista_Instrucciones TkEnd
 	'''
-	if(len(p)==6):
-		p[0]= Node('Condicional', [Node(p[1],[p[2],p[4]], None)], p[5])
+	
+	p[2].changeType('-Guardia: '+str(p[2]))
+	p[4].changeType('-Exito: '+str(p[4]))
+	if (len(p)==6):
+		p[0] = Node('CONDICIONAL', [p[2],p[4]], None)
 	else:
-		p[0]= Node('Condicional', [Node(p[1],[p[2], p[4]]), Node(p[5],[p[7]])], p[8])
+		p[7].changeType('-En otro caso: '+str(p[7]))
+		p[0] = Node('CONDICIONAL', [p[2],p[4],p[7]], None)
 
 
 def p_Inst_Bool(p):
-	'''Inst_Bool : TkWhile Operacion TkHacer Lista_Instrucciones TkOtherwise TkHacer Lista_Instrucciones TkEnd
-	| TkWhile Operacion TkHacer Lista_Instrucciones TkEnd
+	'''Inst_Bool : TkWhile Operacion TkHacer Lista_Instrucciones TkEnd
 	'''
-	if (len(p)==6):
-		p[0]= Node(p[1], [p[2], p[4]], p[1])
-	else:
-		p[0]= Node(p[1], [p[2], p[4], p[7]], p[1])
+	p[2].changeType('-Guardia: '+str(p[2]))
+	p[4].changeType('-Exito: '+str(p[4]))
+	p[1] = Node('WHILE', [p[2],p[4]], None)
+	
 
 def p_Inst_For(p):
-	'''Inst_For : TkFor TkId TkFrom TkNum TkTo TkNum 
-	| TkFor TkId TkFrom TkNum TkTo TkNum TkStep Operacion TkHacer Inst TkEnd
+	'''Inst_For : TkFor TkId TkFrom Expresion TkTo Expresion TkHacer Inst TkEnd
+	| TkFor TkId TkFrom Expresion TkTo Expresion TkStep Expresion TkHacer Inst TkEnd
 	'''
-	if (len(p)==6):
-		p[0]= Node(p[1], [p[2], p[3], p[4], p[5], p[6]], None )
+	p[2] = Node('Variable de iteracion: (var "'+p[2]+'")', None, None)
+	
+	if isinstance(p[4],int):
+		p[4] = Node('-Limite inferior: literal entero ('+str(p[4])+')', None, None)
 	else:
-		pass 
+		p[4]= Node('-Limite inferior: variable ("'+p[4]+'")', None, None)
+	
+	if isinstance(p[6],int):
+		p[6] = Node('-Limite superior: literal entero ('+str(p[6])+')', None, None)
+	else:
+		p[6] = Node('-Limite superior: variable ("'+p[6]+'")', None, None)
+
+
+	if (len(p)==10):
+		p[8].changeType('-Exito: '+str(p[8]))
+		p[0]= Node('ITERACION DETERMINADA', [p[2],p[4],p[6],p[8]], None )
+	else:
+		if isinstance(p[8],int):
+			p[8].changeType('-Step: literal entero ('+str(p[8])+')')
+		else:
+			p[8].changeType('-Step: variable ("'+str(p[8])+'")')
+		p[10].changeType('-Exito:'+str(p[10]))
+		p[0]= Node('ITERACION DETERMINADA', [p[2], p[4], p[6], p[8], p[10]], None )
+
+def p_Expresion(p):
+	'''Expresion : TkNum
+	| TkId'''
+	
+	p[0]=p[1]
+
 
 def p_Operacion(p):
 	'''Operacion : TkParAbre Operacion TkParCierra
@@ -156,81 +186,70 @@ def p_Operacion(p):
 	| Operacion TkIgual Operacion 
 	| Operacion TkDesigual Operacion
 	| Operacion TkConjuncion Operacion
-    | Operacion TkDisyuncion Operacion 
-    | TkNegacion Operacion
-    | OpCaracter
-    | TkId  
-    | TkNum
-    | TkTrue 
-    | TkFalse
-    | TkCaracter
-	'''
-	if (len(p) == 2):
-		p[0]=Node(p[1], None, None)
-	elif (len(p) == 3):
-		p[0]=Node(p[1],  [p[2]], None)
-	else:
-		p[0]=Node(p[2],  [p[1],p[3]], None)
-
-'''
-def p_Operacion(p):
-	Operacion : TkParAbre Operacion TkParCierra
-	| Op_Aritmetica
-	| Op_booleana
-	| OpRelacional
+	| Operacion TkDisyuncion Operacion 
+	| TkNegacion Operacion
 	| OpCaracter
-	
-	p[0] = p[1]
-
-
-def p_Op_Aritmetica(p):
-	Op_Aritmetica : Op_Aritmetica TkSuma Op_Aritmetica 
-	| Op_Aritmetica TkResta Op_Aritmetica 
-	| Op_Aritmetica TkMult Op_Aritmetica 
-	| Op_Aritmetica TkDiv Op_Aritmetica 
-	| Op_Aritmetica TkMod Op_Aritmetica 
-	| TkResta Op_Aritmetica 
-	| TkId
-	| TkNum 
-	
-	if (len(p) == 2):
-		p[0]=Node(p[1], None, None)
-	elif (len(p)==3):
-		p[0]=Node(p[1], [p[2]], None)
-	else:
-		p[0]=Node(p[2], [p[1],p[3]], None)
-		
-	
-def p_Op_booleana(p):
-	Op_booleana : Op_booleana TkConjuncion Op_booleana
-    | Op_booleana TkDisyuncion Op_booleana 
-    | TkNegacion Op_booleana
-	| TkId
-	| TkTrue
-	| TkFalse 
-	
-	if (len(p) == 2):
-		p[0]=Node(p[1], None, None)
-	elif (len(p) == 3):
-		p[0]=Node(p[1], [p[2]], None)
-	else:
-		p[0]=Node(p[2],  [p[1],p[3]], None)
-	
-
-def p_OpRelacional(p):
-	OpRelacional : Op_Aritmetica TkMenor Op_Aritmetica
-	| Op_Aritmetica TkMenorIgual Op_Aritmetica 
-	| Op_Aritmetica TkMayor Op_Aritmetica 
-	| Op_Aritmetica TkMayorIgual Op_Aritmetica 
-	| Op_Aritmetica TkIgual Op_Aritmetica 
-	| Op_Aritmetica TkDesigual Op_Aritmetica 
-	| TkId
+	| TkId  
 	| TkNum
-	if (len(p)==2):
-		p[0]=Node(p[1], None, None)
+	| TkTrue 
+	| TkFalse
+	| TkCaracter
+	'''
+	
+	if (len(p) == 2):
+		if isinstance(p[1],int):
+			p[0] = Node('literal entero ('+str(p[1])+')', None, None)
+		elif (p[1] == 'true'):
+			p[0] = Node('booleano (TRUE)+', None, None)
+		elif (p[1] == 'false'):
+			p[0] = Node('booleano (FALSE)+', None, None)
+		else:
+			caracter = re.compile('[\'][a-zA-Z_][\']|["][a-zA-Z_]["]')
+			if caracter.match(p[1]):
+				p[0] = Node('caracter ('+p[1]+')', None, None)
+			else:
+				p[0] = Node('variable ("'+p[1]+'")', None, None)
+	
+	elif (len(p) == 3):
+		if (p[2] == '-'):
+			p[0]=Node("MENOS UNARIO",  [p[2]], None)
+		else:
+			p[0]=Node("NOT",  [p[2]], None)
+	
 	else:
-		p[0]=Node(p[2],  [p[1],p[3]], None)
-'''
+		if not (p[1]=='('):
+			p[1].changeType('-Operando izquierdo: '+str(p[1]))
+			p[3].changeType('-Operando derecho: '+str(p[3]))
+		else:
+			p[0] = p[2]
+
+		if (p[2] == '+'):
+			p[0] = Node("SUMA", [p[1],p[3]], p[2])
+		elif (p[2] == '-'):
+			p[0] = Node("RESTA", [p[1],p[3]], p[2])
+		elif (p[2] == '*'):
+			p[0] = Node("MULTIPLICACION", [p[1],p[3]], p[2])
+		elif (p[2] == '/'):
+			p[0] = Node("DIVISION", [p[1],p[3]], p[2])
+		elif (p[2] == '%'):
+			p[0] = Node("MODULO", [p[1],p[3]], p[2])
+		elif (p[2] == '<'):
+			p[0]=Node("MENOR QUE", [p[1],p[3]], p[2])
+		elif (p[2] == '<='):
+			p[0]=Node("MENOR O IGUAL QUE", [p[1],p[3]], p[2])
+		elif (p[2] == '>'):
+			p[0]=Node("MAYOR", [p[1],p[3]], p[2])
+		elif (p[2] == '>='):
+			p[0]=Node("MAYOR QUE", [p[1],p[3]], p[2])
+		elif (p[2] == '='):
+			p[0]=Node("IGUAL QUE", [p[1],p[3]], p[2])
+		elif (p[2] == '/='):
+			p[0]=Node("DESIGUAL QUE", [p[1],p[3]], p[2])
+		elif (p[2] == '/\\'):
+			p[0]=Node("CONJUNCION", [p[1],p[3]], p[2])
+		elif (p[2] == '\/'):
+			p[0]=Node("DISYUNCION", [p[1],p[3]], p[2])
+
 def p_Op_Arreglo(p):
 	'''Op_Arreglo : TkId TkCorcheteAbre TkId TkCorcheteCierra 
 	| TkId TkCorcheteAbre TkNum TkCorcheteCierra 
@@ -251,26 +270,27 @@ def p_Inst_Asignacion(p):
 	'''Inst_Asignacion : TkId TkAsignacion Operacion TkPuntoYComa
 	| TkId TkAsignacion Op_Arreglo TkPuntoYComa'''  
 	
-	#p[0] = 'ASIGNACION \n\t - contenedor: VARIABLE \n\t - identificador: ' + str(p[1]) +'\n\t - expresion: '+ tipo + ' \n\t - valor:' + str(p[3])
-	p[0] = Node(p[2], [p[1], p[3]], None)
+	p[1] = Node('-Contenedor: variable ("'+p[1]+'")',None,None)
+	if (isinstance(p[3].type,int)):
+		p[3].changeType('-Expresion: literal entero('+str(p[3])+')')
+	else:
+		p[3].changeType('-Expresion: '+str(p[3]))
+	p[0] = Node('ASIGNACION', [p[1], p[3]], None)
 
 def p_Inst_Salida(p):
 	'''Inst_Salida : TkPrint Operacion TkPuntoYComa
 	| TkPrint Op_Arreglo TkPuntoYComa'''
-	p[0]= Node(p[1], [p[2]], None)
-	#p[0] = 'PRINT ' + p[1] + ' expresion: ' + p[2]
+	p[0]= Node('PRINT', [p[2]], None)
 
 	
 def p_Inst_Entrada(p):
 	'''Inst_Entrada : TkRead TkId TkPuntoYComa '''
-	p[0]= Node(p[1], [p[2]], None)
-	#p[0] = 'READ ' + p[1] + ' identificador: ' + p[2]
+	p[0]= Node('READ', [p[2]], None)
 
 
-# Error rule for syntax errors
-#def p_error(p):
-#	print ("Error de sintaxis en la linea")
-
+#Error rule for syntax errors
+def p_error(p):
+	print ("Error de sintaxis en la linea")
 
 def printTree(nodo, tabs):
 	print('\t'*tabs + str(nodo))
@@ -280,9 +300,19 @@ def printTree(nodo, tabs):
 			if nodo.children[i] != None:
 				printTree(nodo.children[i], tabs+1)
 
-# Build the parser
-parser = yacc.yacc()
 
-s = input('Ingresa: ')
-result = parser.parse(s)
-printTree(result, 0)
+def main():
+	if (len(sys.argv) != 2):
+		print("Usage: python3 parser.py nombreArchivo")
+		return -1
+		
+	# Construyendo el parser
+	parser = yacc.yacc()
+		
+	# Se abre el archivo con permisos de lectura
+	string = str(open(str(sys.argv[1]),'r').read())
+	result = parser.parse(string)
+
+	printTree(result, 0)
+
+main()
