adecb465266f53087e13c19d6a98e7dec723454d
Giulianne17 <gvtr96@gmail.com>
2018-06-23 12:28:53 -0400

    Se modifico la clase Node Se agrego un atributo tipo y las funciones get/set correspondientes. Donde se utilizaba la clase, se le agrego el atributo. Todavia falta resolver cuando son muchas variables declaradas y cuando en operaciones trabajan con TkId.

--- Fase3/parser.py
+++ Fase3/parser.py
@@ -6,13 +6,10 @@ import ply.yacc as yacc
 import ply.lex as lex
 import sys
 import re
-import TablaSimbolos
 from Entrega1 import tokens, obtenerColumna, lexerErrorFound
 
 parserErrorFound=False
-RedeclaracionError=False
-
-tablaPrincipal = TablaSimbolos.TablaSimbolos()
+semanticErrorFound=False
 
 # Indica cuales son el orden de precedencia de los tokens y hacia donde asocian.
 precedence = (
@@ -34,19 +31,26 @@ precedence = (
 
 # Clase node que permite la creacion de los nodel del arbol sintactico.
 class Node:
-	def __init__(self,type,children=None,leaf=None):
+	def __init__(self,type,children=None,leaf=None, tipo = None):
 		self.type = type
 		if children:
 			self.children = children
 		else:
 			self.children = [ ]
 		self.leaf = leaf
+		self.tipo=tipo
 	def __str__(self):
 		return str(self.type)
 
 	#Cambia el type del nodo.
 	def changeType(self,newType):
 		self.type = newType
+
+	def setTipo(self, tipo):
+		self.tipo=tipo
+
+	def getTipo(self):
+		return self.tipo
 
 	#Obtiene el type del nodo.
 	def getType(self):
@@ -82,46 +86,28 @@ def p_Lista_Variables(p):
 	| TkId TkAsignacion Operacion TkComa Lista_Variables
 	| TkId TkAsignacion Operacion TkDosPuntos Tipo
 	| TkId TkDosPuntos Tipo '''
-	
-	if (not tablaPrincipal.pertenece(p[1])):
-		agregar = TablaSimbolos.VariablesDeclaradas(p[1],'')
-		agregar.setLinea(p.lineno(1))
-		agregar.setColumna(obtenerColumna(p.slice[1].lexer.lexdata,p.slice[1]))
-		if (len(p)==6):
-			p[0]= str(Node(p[2], [p[1], p[3]], None)) + ' '+ str(p[5])
-			if (p[4]==':'):
-				agregar.setType(p[5])
-				tablaPrincipal.lista.insert(0,agregar)
-			else:
-				tablaPrincipal.lista.insert(0,agregar)
-		else:
-			p[0]= str(p[1]) +'\n' + str(p[3])
-			if(p[2]==':'):
-				agregar.setType(p[3])
-				tablaPrincipal.lista.insert(0,agregar)
-			else:
-				tablaPrincipal.lista.insert(0,agregar)
-				j= tablaPrincipal.lista.index(agregar)
-				#No sirve en todos los casos
-				tablaPrincipal.lista[j].setType(tablaPrincipal.lista[j-1].type)
+	if (len(p)==6):
+		p[0]= str(Node(p[2], [p[1], p[3]], None, None)) + ' '+ str(p[5])
 	else:
-		global RedeclaracionError
-		RedeclaracionError=True
-		#print(tablaPrincipal.busqueda(p[1]))
-		print("La variable '" + p[1] + "' ha sido declarada mas de una vez")
+		p[0]= str(p[1]) +'\n' + str(p[3])   
 
 # Regla de la gramatica que reconoce todo los tipos de variables.
 def p_Tipo(p):
 	''' Tipo : TkInt 
 	| TkBool 
 	| TkChar
-	| TkArray TkCorcheteAbre OpAritmetica TkCorcheteCierra TkOf Tipo 
-	| TkArray TkCorcheteAbre OpAritmetica TkCorcheteCierra TkOf TkId
+	| TkArray  TkCorcheteAbre Operacion TkCorcheteCierra TkOf TkId 
+	| TkArray  TkCorcheteAbre Operacion TkCorcheteCierra TkOf Tipo
 	'''
 	if (p[1]=='array'):
-		p[0] = Node(p[1], [p[2],p[3],p[4],p[6]], p[5])
+		p[0] = Node(p[1], [p[2],p[3],p[4],p[6]], p[5], p[6].getTipo())
 	else: 
-		p[0]=p[1]
+		if(p[1]=='int'):
+			p[0]=Node(p[1], None,None, 'int')
+		elif (p[1]=='bool'):
+			p[0]=Node(p[1], None,None, 'bool')
+		else:
+			p[0]=Node(p[1], None,None, 'char')
 
 # Regla de la gramatica que reconoce los bloques de instrucciones.
 def p_Bloque_Inst(p):	
@@ -150,10 +136,10 @@ def p_Inst(p):
 def p_Inst_Punto(p):
 	'''Inst_Punto : TkId TkPunto Operacion TkPuntoYComa '''
 	if (isinstance(p[3],int)):
-		p[0]=Node('PUNTO', [Node('-Contenedor: '+p[1]),Node('-Expresion: '+str(p[3]))],None )
+		p[0]=Node('PUNTO', [Node('-Contenedor: '+p[1]),Node('-Expresion: '+str(p[3]))],None, None )
 	else:
 		p[3].changeType('-Expresion: '+str(p[3]))
-		p[0]=Node('PUNTO', [Node('-Contenedor: '+p[1]),p[3]])
+		p[0]=Node('PUNTO', [Node('-Contenedor: '+p[1]),p[3]], None)
 
 # Regla de la gramatica que reconoce todas instrucciones.
 def p_Lista_Instrucciones(p):
@@ -164,7 +150,7 @@ def p_Lista_Instrucciones(p):
 			p[2].addChildren(p[1])
 			p[0] = p[2]
 		else:
-			p[0]=Node('SECUENCIACION', [p[1],p[2]], None)
+			p[0]=Node('SECUENCIACION', [p[1],p[2]], None, None)
 
 	else:
 		p[0] = p[1]
@@ -178,10 +164,10 @@ def p_Inst_If(p):
 	p[2].changeType('-Guardia: '+str(p[2]))
 	p[4].changeType('-Exito: '+str(p[4]))
 	if (len(p)==6):
-		p[0] = Node('CONDICIONAL', [p[2],p[4]], None)
+		p[0] = Node('CONDICIONAL', [p[2],p[4]], None, None)
 	else:
 		p[7].changeType('-Fracaso: '+str(p[7]))
-		p[0] = Node('CONDICIONAL', [p[2],p[4],p[7]], None)
+		p[0] = Node('CONDICIONAL', [p[2],p[4],p[7]], None, None)
 
 # Regla de la gramatica que reconoce todas instrucciones de ciclos o while.
 def p_Inst_Bool(p):
@@ -189,14 +175,14 @@ def p_Inst_Bool(p):
 	'''
 	p[2].changeType('-Guardia: '+str(p[2]))
 	p[4].changeType('-Exito: '+str(p[4]))
-	p[0] = Node('CICLO', [p[2],p[4]], None)
+	p[0] = Node('CICLO', [p[2],p[4]], None, None)
 	
 # Regla de la gramatica que reconoce todas instrucciones de iteracion determinada (For).
 def p_Inst_For(p):
 	'''Inst_For : TkFor TkId TkFrom Operacion TkTo Operacion TkHacer Inst TkEnd
 	| TkFor TkId TkFrom Operacion TkTo Operacion TkStep Operacion TkHacer Inst TkEnd
 	'''
-	p[2] = Node('Variable de iteracion: (var "'+p[2]+'")', None, None)
+	p[2] = Node('Variable de iteracion: (var "'+p[2]+'")', None, None, None)
 	
 	p[4].changeType('-Limite inferior: '+str(p[4]))
 	
@@ -204,127 +190,100 @@ def p_Inst_For(p):
 
 	if (len(p)==10):
 		p[8].changeType('-Exito: '+str(p[8]))
-		p[0]= Node('ITERACION DETERMINADA', [p[2],p[4],p[6],p[8]], None )
+		p[0]= Node('ITERACION DETERMINADA', [p[2],p[4],p[6],p[8]], None, None)
 	else:
 		p[8].changeType('-Step: '+str(p[8]))
 		p[10].changeType('-Exito: '+str(p[10]))
-		p[0]= Node('ITERACION DETERMINADA', [p[2], p[4], p[6], p[8], p[10]], None )
-
+		p[0]= Node('ITERACION DETERMINADA', [p[2], p[4], p[6], p[8], p[10]], None, None)
 
 # Regla de la gramatica que reconoce todas las operaciones del lenguaje.
 def p_Operacion(p):
-	'''Operacion : OpAritmetica
-	| OpRelacional
-	| OpBoolena
+	'''Operacion : TkParAbre Operacion TkParCierra
+	| Operacion TkSuma Operacion 
+	| Operacion TkResta Operacion 
+	| Operacion TkMult Operacion 
+	| Operacion TkDiv Operacion 
+	| Operacion TkMod Operacion 
+	| TkResta Operacion 
+	| Operacion TkMenor Operacion
+	| Operacion TkMenorIgual Operacion 
+	| Operacion TkMayor Operacion 
+	| Operacion TkMayorIgual Operacion 
+	| Operacion TkIgual Operacion 
+	| Operacion TkDesigual Operacion
+	| Operacion TkConjuncion Operacion
+	| Operacion TkDisyuncion Operacion 
+	| TkNegacion Operacion
 	| OpCaracter
 	| Op_Arreglo
-	'''
-	p[0] = p[1]
-
-def p_OpAritmetica(p):
-	'''OpAritmetica : TkParAbre OpAritmetica TkParCierra
-	| OpAritmetica TkSuma OpAritmetica 
-	| OpAritmetica TkResta OpAritmetica 
-	| OpAritmetica TkMult OpAritmetica 
-	| OpAritmetica TkDiv OpAritmetica 
-	| OpAritmetica TkMod OpAritmetica 
-	| TkResta OpAritmetica 
 	| TkId  
 	| TkNum
-	'''
-	if (len(p) == 2):
-		if isinstance(p[1],int):
-			p[0] = Node('literal entero ('+str(p[1])+')', None, None)
-		else:
-			p[0] = Node('variable ("'+p[1]+'")', None, None)
-	
-	elif (len(p) == 3):
-		p[0]=Node("MENOS UNARIO",  [p[2]], None)
-
-	else:
-		if not (p[1]=='('):
-			p[1].changeType('-Operando izquierdo: '+str(p[1]))
-			p[3].changeType('-Operando derecho: '+str(p[3]))
-		else:
-			p[0] = p[2]
-		
-		if (p[2] == '+'):
-			p[0] = Node("SUMA", [p[1],p[3]], p[2])
-		elif (p[2] == '-'):
-			p[0] = Node("RESTA", [p[1],p[3]], p[2])
-		elif (p[2] == '*'):
-			p[0] = Node("MULTIPLICACION", [p[1],p[3]], p[2])
-		elif (p[2] == '/'):
-			p[0] = Node("DIVISION", [p[1],p[3]], p[2])
-		elif (p[2] == '%'):
-			p[0] = Node("MODULO", [p[1],p[3]], p[2])
-
-
-def p_OpRelacional(p):
-	'''OpRelacional : TkParAbre OpRelacional TkParCierra
-	| OpRelacional TkMenor OpRelacional
-	| OpRelacional TkMenorIgual OpRelacional 
-	| OpRelacional TkMayor OpRelacional 
-	| OpRelacional TkMayorIgual OpRelacional 
-	| OpRelacional TkIgual OpRelacional 
-	| OpRelacional TkDesigual OpRelacional
-	| OpAritmetica
-	'''
-	if (len(p) == 2):
-		p[0] = p[1]
-	
-	else:
-		if not (p[1]=='('):
-			p[1].changeType('-Operando izquierdo: '+str(p[1]))
-			p[3].changeType('-Operando derecho: '+str(p[3]))
-		else:
-			p[0] = p[2]
-		
-		if (p[2] == '<'):
-			p[0]=Node("MENOR QUE", [p[1],p[3]], p[2])
-		elif (p[2] == '<='):
-			p[0]=Node("MENOR O IGUAL QUE", [p[1],p[3]], p[2])
-		elif (p[2] == '>'):
-			p[0]=Node("MAYOR", [p[1],p[3]], p[2])
-		elif (p[2] == '>='):
-			p[0]=Node("MAYOR QUE", [p[1],p[3]], p[2])
-		elif (p[2] == '='):
-			p[0]=Node("IGUAL QUE", [p[1],p[3]], p[2])
-		elif (p[2] == '/='):
-			p[0]=Node("DESIGUAL QUE", [p[1],p[3]], p[2])
-
-def p_OpBoolena(p):
-	'''OpBoolena : TkParAbre OpBoolena TkParCierra
-	| OpBoolena TkConjuncion OpBoolena
-	| OpBoolena TkDisyuncion OpBoolena 
-	| TkNegacion OpBoolena  
 	| TkTrue 
 	| TkFalse
-	| OpRelacional
+	| TkCaracter
 	'''
-
+	
 	if (len(p) == 2):
-		if isinstance(p[1],Node):
-			p[0] = p[1]
+		if isinstance(p[1],int):
+			p[0] = Node('literal entero ('+str(p[1])+')', None, None, 'int')
 		elif (p[1] == 'true'):
-			p[0] = Node('booleano (TRUE)', None, None)
+			p[0] = Node('booleano (TRUE)+', None, None, 'bool')
 		elif (p[1] == 'false'):
-			p[0] = Node('booleano (FALSE)', None, None)
+			p[0] = Node('booleano (FALSE)+', None, None, 'bool')
+		elif isinstance(p[1],Node):
+			p[0] = p[1]
+		else:
+			caracter = re.compile('[\'][a-zA-Z_][\']|["][a-zA-Z_]["]')
+			if caracter.match(p[1]):
+				p[0] = Node('caracter ('+p[1]+')', None, None, 'char')
+			else:
+				p[0] = Node('variable ("'+p[1]+'")', None, None, None) # p[1].getTipo())    
 	
 	elif (len(p) == 3):
-		p[0]=Node("NOT",  [p[2]], None)
-
+		if (p[1] == '-'):
+			p[0]=Node("MENOS UNARIO",  [p[2]], None, 'int')
+		else:
+			p[0]=Node("NOT",  [p[2]], None, 'bool')
+	
 	else:
 		if not (p[1]=='('):
 			p[1].changeType('-Operando izquierdo: '+str(p[1]))
 			p[3].changeType('-Operando derecho: '+str(p[3]))
 		else:
 			p[0] = p[2]
-		
-		if (p[2] == '/\\'):
-			p[0]=Node("CONJUNCION", [p[1],p[3]], p[2])
+
+		'''
+		if (p[1].getTipo()!=p[3].getTipo()):
+			global semanticErrorFound
+			semanticErrorFound=True
+		'''
+
+		if (p[2] == '+'):
+			p[0] = Node("SUMA", [p[1],p[3]], p[2], 'int')
+		elif (p[2] == '-'):
+			p[0] = Node("RESTA", [p[1],p[3]], p[2], 'int')
+		elif (p[2] == '*'):
+			p[0] = Node("MULTIPLICACION", [p[1],p[3]], p[2], 'int')
+		elif (p[2] == '/'):
+			p[0] = Node("DIVISION", [p[1],p[3]], p[2], 'int')
+		elif (p[2] == '%'):
+			p[0] = Node("MODULO", [p[1],p[3]], p[2], 'int')
+		elif (p[2] == '<'):
+			p[0]=Node("MENOR QUE", [p[1],p[3]], p[2], 'bool')
+		elif (p[2] == '<='):
+			p[0]=Node("MENOR O IGUAL QUE", [p[1],p[3]], p[2], 'bool')
+		elif (p[2] == '>'):
+			p[0]=Node("MAYOR", [p[1],p[3]], p[2], 'bool')
+		elif (p[2] == '>='):
+			p[0]=Node("MAYOR QUE", [p[1],p[3]], p[2], 'bool')
+		elif (p[2] == '='):
+			p[0]=Node("IGUAL QUE", [p[1],p[3]], p[2], 'bool')
+		elif (p[2] == '/='):
+			p[0]=Node("DESIGUAL QUE", [p[1],p[3]], p[2], 'bool')
+		elif (p[2] == '/\\'):
+			p[0]=Node("CONJUNCION", [p[1],p[3]], p[2], 'bool')
 		elif (p[2] == '\/'):
-			p[0]=Node("DISYUNCION", [p[1],p[3]], p[2])
+			p[0]=Node("DISYUNCION", [p[1],p[3]], p[2], 'bool')
 
 # Regla de la gramatica que reconoce todas las operaciones de los arreglos.
 def p_Op_Arreglo(p):
@@ -367,33 +326,38 @@ def p_Op_Arreglo(p):
 
 # Regla de la gramatica que reconoce todas las operaciones con los caracteres.
 def p_OpCaracter(p):
-	'''OpCaracter : TkParAbre OpCaracter TkParCierra
+	'''OpCaracter : TkCaracter TkSiguienteCar 
+	| TkCaracter TkAnteriorCar 
+	| TkValorAscii TkCaracter
+	| TkId TkSiguienteCar 
+	| TkId TkAnteriorCar 
+	| TkValorAscii TkId
 	| OpCaracter TkSiguienteCar 
 	| OpCaracter TkAnteriorCar 
 	| TkValorAscii OpCaracter
-	| TkCaracter
-	| TkId
 	'''
-
-	if (len(p)==2):
-		caracter = re.compile('[\'][a-zA-Z_][\']|["][a-zA-Z_]["]')
-		if caracter.match(p[1]):
+	caracter = re.compile('[\'][a-zA-Z_][\']|["][a-zA-Z_]["]')
+	if (p[1]=='#'):
+		if isinstance(p[2],Node):
+			p[2].changeType('-Caracter: '+str(p[2]))
+		elif caracter.match(p[2]):
+			p[2] = Node('-Caracter: '+str(p[2]), None,None, 'char')
+		else:
+			p[2] = Node('-Variable: '+str(p[2]), None,None, None)
+		p[0] = Node('VALOR ASCII',[p[2]], None, p[2])
+	
+	else:
+		if isinstance(p[1],Node):
+			p[1].changeType('-Caracter: '+str(p[1]))
+		elif caracter.match(p[1]):
 			p[1] = Node('-Caracter: '+str(p[1]))
 		else:
 			p[1] = Node('-Variable: '+str(p[1]))
-
-	elif (len(p)==3):
-		if (p[1]=='#'):
-			p[0] = Node('VALOR ASCII',[p[2]])
-		elif (p[2]=='[+][+]'):
-			p[0] = Node('CARACTER SIGUIENTE',[p[1]])
+		
+		if (p[2]=='[+][+]'):
+			p[0] = Node('CARACTER SIGUIENTE',[p[1]], None, None)
 		else:
-			p[0] = Node('CARACTER ANTERIOR',[p[1]])
-	
-	else:
-		p[0] = p[2]
-				
-
+			p[0] = Node('CARACTER ANTERIOR',[p[1]], None, None)
 
 # Regla de la gramatica utilizada para reconocer una asignacion
 def p_Inst_Asignacion(p):
@@ -405,7 +369,7 @@ def p_Inst_Asignacion(p):
 		p[3].changeType('-Expresion: literal entero('+str(p[3])+')')
 	else:
 		p[3].changeType('-Expresion: '+str(p[3]))
-	p[0] = Node('ASIGNACION', [p[1], p[3]], None)
+	p[0] = Node('ASIGNACION', [p[1], p[3]], None, p[3].getTipo())
 
 # Regla de la gramatica que reconoce los print
 def p_Inst_Salida(p):
@@ -448,9 +412,8 @@ def main():
 	result = parser.parse(string)
 	
 	#Si no hay errores, imprime el arbol.
-	if (not lexerErrorFound) and (not parserErrorFound) and (not RedeclaracionError):
+	if (not lexerErrorFound) and (not parserErrorFound) and (not semanticErrorFound):
 		printTree(result, 0)
-		#print(tablaPrincipal)
 
 #Llamada a la funcion
 main()
